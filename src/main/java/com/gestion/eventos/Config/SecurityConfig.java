cadb886bc0341b934a1ac87d45694e6dfe5c18e3
sharon0695 <shzuray@gmail.com>
2025-10-19 17:22:10 -0500

    Modificaciones configuración

--- src/main/java/com/gestion/eventos/Config/SecurityConfig.java
+++ src/main/java/com/gestion/eventos/Config/SecurityConfig.java
@@ -2,14 +2,13 @@ package com.gestion.eventos.Config;
 
 import com.gestion.eventos.Repository.IUsuarioRepository;
 import com.gestion.eventos.Security.JwtAuthenticationFilter;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
