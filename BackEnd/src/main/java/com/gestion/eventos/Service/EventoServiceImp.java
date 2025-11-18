f666354371b7714584b3fc44e0a5d2149c6d8317
Negrito <davidcopete123@gmail.com>
2025-11-17 19:07:18 -0500

    hi

--- BackEnd/src/main/java/com/gestion/eventos/Service/EventoServiceImp.java
+++ BackEnd/src/main/java/com/gestion/eventos/Service/EventoServiceImp.java
@@ -5,7 +5,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.sql.Date;
 import java.sql.Time;
 
 import org.springframework.beans.factory.annotation.Autowired;
